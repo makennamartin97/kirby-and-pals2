da1e4f8a1b2a629511176274f03d1afa1630aac9
makennamartin97 <makennamartin97@yahoo.com>
2021-04-23 16:57:18 -0400

    line height fix

--- src/components/Section2.jsx
+++ src/components/Section2.jsx
@@ -15,10 +15,10 @@ class Section2 extends Component {
                 
             </div>
             <div className="row nopadding">
-              <div className="col-sm-12 col-md-5 nopadding">
+              <div className=" col-xs-12 col-sm-12 col-md-5 nopadding">
                 <img src={yard} alt="Dog" id="dog" />
               </div>
-              <div className="col-sm-12 col-md-7 content">
+              <div className="col-xs-12 col-sm-12 col-md-7 content">
                 <h6><u>Hello, my name is Rachel.</u></h6>
                 <p>I started Kirby & Pals in 2020, after working in corporate retail for 5 years and finding myself wanting to do something more, something I loved!  Creating Kirby & Pals has given me the opportunity to share my love and passion for animals with others. I want to create an experience where owners feel I am an extended part of their family, and comfortable knowing their four-legged loved friends are safe, supervised, and loved when they are in my care. </p>
                 <img src={twodogs} alt="Dog" id="dogs" />
